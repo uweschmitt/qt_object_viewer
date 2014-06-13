7700bc8eb06348b41ce7f3885d583a9df64f31ac
Uwe Schmitt <uwe.schmitt@id.ethz.ch>
2014-06-13 17:08:38 +0200

    released 1.0.0

--- qt_object_viewer/__init__.py
+++ qt_object_viewer/__init__.py
@@ -1,3 +1,5 @@
 from dialog import ObjectTreeDialog
 from tree_widget import ObjectTreeWidget
 from tree import Node, Leaf
+
+__version__ = (1, 0, 0)
--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from setuptools import setup, find_packages
 
-version = (0, 0, 1)
+description = "qt tree model + tree viewer for nested objects"
 
-description = "qt tree model + tree viewre for nestes objects"
+long_description = """
+This Python package provides a Qt based widget and a dialog class for exploring a
+nested object structure.
+
+For examples see: http://github.com/uweschmitt/qt_object_viewer.git
+"""
 
 setup(name="qt_object_viewer",
       description=description,
+      long_description=long_description,
       maintainer="Uwe Schmitt",
       maintainer_email="uwe.schmitt@id.ethz.ch",
       platforms=["any"],
-
+      version="1.0.0",
       packages=find_packages(exclude=["tests"]),
-      #entry_points={
-          #"gui_scripts": ["envipy = eawag_gui.main:main"]
-      #},
-      version="%d.%d.%d" % version,
       include_package_data=True,
       zip_safe=False,
-      install_requires=["guidata"]
       )
